119ae3adc926f23bdad64525e268bf92fed5c0f5
pheeria <olzhas.askar@gmail.com>
2017-02-22 22:46:22 +0100

    Added decipher function

--- vigenere.c
+++ vigenere.c
@@ -4,43 +4,22 @@
 
 void key_overflow(int *key_counter, int key_length);
 int changer(int character);
+void cipher(char *text, char *key);
+void decipher(char *text, char *key);
 
 int main(int argc, char *argv[])
 {
     char* vigenere = argv[1];
-    char* cipher = argv[2];
-
-    int vlen = strlen(vigenere);
-    int clen = strlen(cipher);
+    char* pcipher = argv[2];
 
     printf("vigenere: %s\n", vigenere);
-    printf("cipher: %s\n", cipher);
+    printf("cipher: %s\n", pcipher);
 
-    for (int i = 0, j = 0, to_add = 0; i < vlen; i++, j++)
-    {
-	key_overflow(&j, clen);
-        to_add = changer(cipher[j]);
-
-	if (isupper(vigenere[i]))
-	{
-	    vigenere[i] += to_add;
-	    if (!isupper(vigenere[i]))
-	    {
-		vigenere[i] -= 26;
-	    }
-	}
-	else if (islower(vigenere[i]))
-	{
-	    vigenere[i] += to_add;
-	    if (!islower(vigenere[i]))
-	    {
-		vigenere[i] -= 26;
-	    }
-	}
-    }
-
+    cipher(vigenere, pcipher);
     printf("ciphered: %s\n", vigenere);
 
+    decipher(vigenere, pcipher);
+    printf("deciphered: %s\n", vigenere);
 }
 
 void key_overflow(int *key_counter, int key_length)
@@ -66,4 +45,60 @@ int changer(int character)
 	result = 0;
     }
     return result;
+}
+void cipher(char *text, char *key)
+{
+    int vlen = strlen(text);
+    int clen = strlen(key);
+
+    for (int i = 0, j = 0, to_add = 0; i < vlen; i++, j++)
+    {
+	key_overflow(&j, clen);
+        to_add = changer(key[j]);
+
+	if (isupper(text[i]))
+	{
+	    text[i] += to_add;
+	    if (!isupper(text[i]))
+	    {
+		text[i] -= 26;
+	    }
+	}
+	else if (islower(text[i]))
+	{
+	    text[i] += to_add;
+	    if (!islower(text[i]))
+	    {
+		text[i] -= 26;
+	    }
+	}
+    }
+}
+void decipher(char *text, char *key)
+{
+    int vlen = strlen(text);
+    int clen = strlen(key);
+
+    for (int i = 0, j = 0, to_add = 0; i < vlen; i++, j++)
+    {
+	key_overflow(&j, clen);
+        to_add = changer(key[j]);
+
+	if (isupper(text[i]))
+	{
+	    text[i] -= to_add;
+	    if (!isupper(text[i]))
+	    {
+		text[i] += 26;
+	    }
+	}
+	else if (islower(text[i]))
+	{
+	    text[i] -= to_add;
+	    if (!islower(text[i]))
+	    {
+		text[i] += 26;
+	    }
+	}
+    }
 }
