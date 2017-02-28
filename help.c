7af04897f631ee30a3acbb3bb48725da75bf3873
pheeria <olzhas.askar@gmail.com>
2017-02-28 21:34:08 +0100

    Expanded help

--- help.c
+++ help.c
@@ -7,9 +7,13 @@ void help(void)
 {
     printf("\
 Usage:\n\
-\t asco file [options]...\n\
+     asco file [options]...\n\
 Options:\n\
-\t -o, --output file\t specify a custom output file name\n\
-\t -h, --help\t show this help\n");
+     -o, --output file         specify a custom output file name\n\
+     -h, --help                show this help\n\
+     -a, --atbash              use Atbash cipher\n\
+     -v, --vigenere cipher     use Vigenere cipher, requires\n\
+     --encipher                encipher mode, set by default\n\
+     --decipher                switch to decipher mode\n");
     exit(0);
 }
